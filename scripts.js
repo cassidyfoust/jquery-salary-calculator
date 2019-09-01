d17c82991f6f73b505f3bce788f3ca54883d78d8
Cassidy Foust <cassidy.foust@gmail.com>
2019-09-01 09:43:07 -0500

    added a color scheme

--- scripts.js
+++ scripts.js
@@ -14,6 +14,9 @@ function newRow(first, last, id, title, salary) {
         monthlyFees = parseInt(monthlyFees) + parseInt(newEmployee.salary)/12;
     let $el = $('#employeesTable');
     $el.append(`<tr><td>` + newEmployee.first + `</td><td>` + newEmployee.last + `</td><td>` + newEmployee.id + `</td><td>` + newEmployee.title + `</td><td id="empSal">` + newEmployee.salary + `</td><td><button id="delete" onClick="removeEmployee(this); removeSalary(` + newEmployee.salary + `);">Delete</button></td></tr>`);
+    
+    $("tr:even").css("background-color", "#A39171");
+    $("tr:odd").css("background-color", "#ABC4AB");
 
     $('#firstName').val('');
     $('#lastName').val('');
@@ -38,7 +41,6 @@ function totalMonthlyCalc(){
 
 function removeSalary(value){
     let removedSalary = value;
-    console.log(removedSalary);
     removedSalary = removedSalary/12;
     monthlyFees = parseInt(monthlyFees) - parseInt(removedSalary);
     totalMonthlyCalc();
@@ -46,9 +48,11 @@ function removeSalary(value){
 
 function removeEmployee(employee){
     $(employee).parent().parent().remove();
+
+    $("tr:even").css("background-color", "#ff9191");
+    $("tr:odd").css("background-color", "#ffcfcf");
 }
 
-// let faveFood = $(this).parent().data('favoriteFood')
 
 function readyNow() {
     $('#submitButton').on('click', function(){
